ad2cf153fbb8c31606e6fc371bcd7f0fbe960ef3
Alexey Hrynevich <a.hrynevich@sumatosoft.com>
2019-07-25 14:51:58 +0300

    Add some changes on BE

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,11 +7,8 @@ class ConversationsController < ApplicationController
   def create
     conversation = Conversation.new(conversation_params)
     if conversation.save
-      serialized_data = ActiveModelSerializers::Adapter::Json.new(
-        ConversationSerializer.new(conversation)
-      ).serializable_hash
-      ActionCable.server.broadcast 'conversations_channel', serialized_data
-      head :ok
+      serialized_data = ConversationSerializer.new(conversation).as_json
+      ActionCable.server.broadcast('conversations_channel', conversation: serialized_data)
     end
   end
   
